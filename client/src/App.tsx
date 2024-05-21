5495aa1251f6dc59d9d68abf63658c29831b4bc4
zhuckov <zhuckovkj@gmail.com>
2024-05-21 15:51:02 +0300

    create admin panel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/header/Header";
 import { FC } from "react";
 import Catalog from "./components/pages/Catalog";
 import Home from "./components/pages/Home";
 import ErrorPage from "./components/pages/ErrorPage";
+import Admin from "./components/pages/Admin";
 
 const App: FC = () => {
+  const location = useLocation();
+  const isAdmin = location.pathname == "/admin-panel";
   return (
-    <div className="max-w-screen-ds mx-auto">
-      <Header />
+    <div className={isAdmin ? "" : "max-w-screen-ds mx-auto"}>
+      {!isAdmin && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
+        <Route path="/admin-panel" element={<Admin />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
---
+++ client/src/components/pages/Admin.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Admin: FC = () => {
+  return <div>Админка</div>;
+};
+
+export default Admin;
